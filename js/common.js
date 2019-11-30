eeccf3d0a61e911cc7687899686f066748c8d044
Dmitriy Filippov <Amoralis.19@gmail.com>
2019-11-30 06:24:23 +0300

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  jQuery('li.has-children').mouseenter(function() {
+    jQuery(this).children('ul').slideDown(500);
+  });
+  
+  jQuery('.has-children').mouseleave(function() {
+    jQuery(this).children('ul').slideUp(500);
+  });
+
+  jQuery('.new-left-wrap').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+      arrows: true,
+      dots: true,
+      autoplay: true,
+      autoplayspeed: 4000,
+      speed: 500
+  });
+  jQuery('.sale-slider').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    infinite: true,
+      arrows: true,
+      dots: false,
+      autoplay: true,
+      autoplayspeed: 5000,
+      speed: 500
+  });
+});
+
+
